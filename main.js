7f62aeccda86b4ee4375077d7b77f615d408df2a
Agnes Dev <agnesdev@MacBook-Air-de-Agnes.local>
2023-12-12 23:36:53 -0500

    ajout des fichiers index.html  main.js   style.css

---
+++ main.js
@@ -0,0 +1,10 @@
+// Ceci est un commentaire JavaScript
+console.log("Bonjour depuis main.js");
+
+// Exemple de fonction
+function afficherMessage() {
+    alert("Bienvenue sur mon site web !");
+}
+
+// Appel de la fonction
+afficherMessage();
